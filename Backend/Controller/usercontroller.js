72778f5d807c1025d73475969c451279b422695b
PrgDominique <dominiquesimbillo10@gmail.com>
2022-09-19 20:10:25 +0800

    login process

---
+++ Backend/Controller/usercontroller.js
@@ -0,0 +1,25 @@
+const User = require('../model/userModel')
+
+
+// login user
+const loginUser = async (req, res)=> {
+    res.json({mssg: 'login user'})
+}
+
+//sign up user
+
+const signupUser = async (req, res)=> {
+    const {email, password} = req.body
+
+    try{
+        const user = await User.signup(email, password)
+
+        res.status(200).json({email, user})
+    } catch (error){
+        res.status(400).json({error: error.message})
+    }
+
+    res.json({mssg: 'signup user'})
+}
+
+module.exports = { loginUser, signupUser}
---
+++ Backend/server.js
@@ -0,0 +1,34 @@
+
+require('dotenv').config()
+
+const express = require('express')
+const exerciseroutes = require('./routes/exercise')
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/user')
+
+const app = express()
+
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/api/exercise', exerciseroutes)
+app.use('/api/user', userRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => { 
+    app.listen(process.env.PORT , () => {
+      console.log('Connected to database & listening port', process.env.PORT)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
+
+
+
+
